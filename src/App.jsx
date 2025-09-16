29a2fdd3a97785835f8d793943594aa298615ff0
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-16 19:33:18 +0530

    Add React components: Dakshina for event donations and Player for sports stats

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ function App() {
   return (
     <>
       <h1> React Core Concepts</h1>
-      <Person></Person>
+      {/* <Person></Person>
       <Sports></Sports>
       <Student></Student>
       <Developer name="rajon" tech="js"></Developer>
       <Developer name="Majumdar" tech="python"></Developer>
       <Developer name="Shabana"></Developer>
+      <Player name="Rini" runs="5000"></Player>
+      <Player name="Mini" runs="5000"></Player>
+      <Dakshina event="chada for durgapuja" amount="2500"></Dakshina>
+      <Dakshina event="chada for kalipuja"></Dakshina> */}
     </>
   );
 }
@@ -26,6 +30,30 @@ const personStyle = {
   color: "red",
   textAlign: "left",
 };
+
+function Dakshina({ event, amount = 0 }) {
+  return (
+    <div className="student">
+      <p>Dakshina For:{event} </p>
+      <p>Amount: {amount} </p>
+    </div>
+  );
+}
+
+const { name, runs } = { name: "Rini", runs: "5000" };
+
+function Player({ name, runs }) {
+  return (
+    <div className="student">
+      <h3>Name: {props.name} </h3>
+      <p>Runs: {props.runs}</p>
+    </div>
+  );
+}
+// const { name, tech } = {
+//   name: "Majumdar",
+//   tech: "js",
+// };
 
 function Developer(props) {
   console.log(props);
