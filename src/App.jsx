a17992b414f634bc230846681a7367bf92f1b751
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-16 17:11:52 +0530

    Added Student component and Developer props with conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,50 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1> React Core Concepts</h1>
       <Person></Person>
       <Sports></Sports>
+      <Student></Student>
+      <Developer name="rajon" tech="js"></Developer>
+      <Developer name="Majumdar" tech="python"></Developer>
+      <Developer name="Shabana"></Developer>
     </>
   );
 }
+function Student() {
+  return (
+    <div className="student">
+      <p>Name: </p>
+      <p>Dept: </p>
+    </div>
+  );
+}
+
+const personStyle = {
+  color: "red",
+  textAlign: "left",
+};
+
+function Developer(props) {
+  console.log(props);
+  return (
+    <div
+      style={{
+        border: "2px solid green",
+        borderRadius: "20px",
+      }}
+    >
+      <h3>Developer: {props.name}</h3>
+      <p>Technology: {props.tech}</p>
+    </div>
+  );
+}
+
 function Person() {
   const age = 19;
   const name = "Rimpa";
   return (
-    <p>
+    <p id="" title="tooltip" style={personStyle}>
       I am person: {name} {age}
     </p>
   );
