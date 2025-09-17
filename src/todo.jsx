8e535cfb11a36b326e6899f8c9afac2bb8124385
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-17 18:09:12 +0530

    Add time duration display for completed tasks

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,12 +1,22 @@
-export default function ToDo({ task, isDone }) {
-  props.task = "JS";
-  return <li>Task:{task} </li>;
-}
+// export default function ToDo({ task, isDone }) {
+//   props.task = "JS";
+//   return <li>Task:{task} </li>;
+// }
 
-export default function ToDo({ task, isDone }) {
+// export default function ToDo({ task, isDone }) {
+//   if (isDone === true) {
+//     return <li>Done : {task}</li>;
+//   } else {
+//     return <li>Pending: {task} </li>;
+//   }
+// }
+export default function ToDo({ task, isDone, time = 0 }) {
   if (isDone === true) {
-    return <li>Done : {task}</li>;
-  } else {
-    return <li>Pending: {task} </li>;
+    return (
+      <li>
+        Done : {task} Duration:{time}
+      </li>
+    );
   }
+  return <li>To be done: {task} </li>;
 }
